2233aabb9bd3e56d64add0086acde9c1a11bda4f
AntonBen <anton_1891@hotmail.com>
2021-11-02 14:27:47 +0100

    init foodPicker

---
+++ App.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import FoodSwiper from './screens/FoodSwiper';
+
+export default function App() {
+  return <FoodSwiper/>
+  
+}
---
+++ screens/FoodSwiper.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function FoodSwiper() {
+  return  <View style={styles.container}>
+      <View style={styles.picture}></View>
+        <View style={styles.foodInfo}>
+            <Text>
+              Babas
+            </Text>
+        </View>
+        <View style={styles.info}>
+            <Text>hej</Text>
+        </View>
+    </View>
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+  },
+  picture:{
+    flex: 2,
+  },
+  foodInfo: {
+    backgroundColor: '#ff00ff',
+    padding: 10,
+    width: '100%',
+  },
+  info: {
+    backgroundColor: '#ffff00',
+    flex: 1,
+    width: '100%'
+  },
+
+});
